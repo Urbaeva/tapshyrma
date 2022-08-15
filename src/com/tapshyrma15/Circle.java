189092c2d2aceb0972f474f7b284b5537f8dead2
Burulai <burulaiurbaeva13@gmail.com>
2022-08-15 22:43:27 +0600

    tapshyrma15

--- src/com/tapshyrma15/Circle.java
+++ src/com/tapshyrma15/Circle.java
@@ -1,18 +1,18 @@
 package com.tapshyrma15;
 
 public class Circle {
-    private final double PI = 3.14;
-    private int radius;
+    private static final double PI = 3.14;
+    private static int radius;
 
     public Circle(int radius) {
-        this.radius = radius;
+        Circle.radius = radius;
     }
 
-    public void area(){
+    public static void area(){
         System.out.println(PI*radius*radius);
     }
 
-    public void circumference(){
+    public static void circumference(){
         System.out.println(2*PI*radius);
     }
 
--- src/com/tapshyrma15/Main.java
+++ src/com/tapshyrma15/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    Circle circle = new Circle(4);
-        circle.area();
-        circle.circumference();
+        Circle.area();
+        Circle.circumference();
     }
 }
